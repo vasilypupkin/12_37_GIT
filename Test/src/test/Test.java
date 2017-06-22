526a4a5d0568953f0f5e49389e26e41da524177e
BASKO_D <BASKO_D@BASKO-DS.iba>
2017-06-22 14:24:18 +0300

    a

--- Test/src/test/Test.java
+++ Test/src/test/Test.java
@@ -6,12 +6,7 @@ public class Test {
 		// TODO Auto-generated method stub
 		// change on gitHub
 		// change2 on gitHub
-		// change3 on gitHub
-		// local change 1 
-		// bbb
-		// change4 on gitHub
-		//aaa
-		//ddd
+
 	}
 
 }
