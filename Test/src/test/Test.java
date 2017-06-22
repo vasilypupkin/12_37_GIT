411b97724929fd5205d80823522347cecb38e9fc
BASKO_D <BASKO_D@BASKO-DS.iba>
2017-06-22 14:15:46 +0300

    dev1

--- Test/src/test/Test.java
+++ Test/src/test/Test.java
@@ -4,12 +4,7 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		// change on gitHub
-		// change2 on gitHub
-		// change3 on gitHub
-		// local change 1 
-		// bbb
-		// change4 on gitHub
+
 		//aaa
 	}
 
